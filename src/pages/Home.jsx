3e437a643baae1e0cb022055cb388797b32a649e
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2024-10-09 19:21:33 +0300

    hi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,14 +6,13 @@ function Home() {
         <div class="grid grid-rows-4 grid-flow-col">
 
             <div className='text-xl md:text-2xl lg:text-3xl'>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <p>Hello Necmettinyo</p>
 
-                </p>
             </div>
             <div style={{ fontSize: '250px' }}>22:58:01</div>
 
 
-        </div>
+        </div >
     )
 }
 
