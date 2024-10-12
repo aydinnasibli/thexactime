b7978d23e526a81cecfbf94cb3ff4a914edc4e95
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2024-10-12 16:43:14 +0300

    opblic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
             <div className="mx-auto max-w-8xl px-4 sm:px-6 lg:px-8">
                 <div className="grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-6 gap-3 md:gap-8 py-10 max-sm:max-w-sm max-sm:mx-auto gap-y-8">
                     <div className="col-span-full mb-10 lg:col-span-2 lg:mb-0">
-                        <a href="https://pagedone.io/" className="flex justify-center lg:justify-start">
+                        <a href="https://thexactime.com/" className="flex justify-center lg:justify-start">
                             <img
                                 alt=""
                                 src="../public/cropped_image.ico"
